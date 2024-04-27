fa5b0d69dc6b886ae6b9b1dd6f1fd8f3d83542a7
Jawk <jasonawume5@gmail.com>
2024-04-27 15:47:23 +0000

    COMMIT

---
+++ Vue_Folder/vue-twitter-clone/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+     <Tweet v-for="tweet in tweets" 
+     :key="tweet._id" :tweet="tweet"/>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import Tweet from '../components/Tweet.vue'
+import vweets from '../vweets'
+export default {
+    components: {Tweet},
+    setup () {
+      const tweets = ref(vweets)
+      console.log(tweets)
+      return {
+        tweets
+      }
+    }
+}
+</script>
